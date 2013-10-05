30d590bc7b6d9c7f749300034afa26eabdf905b9
Smeedge <collin@cowfishproductions.com>
2013-10-05 16:29:28 -0400

    Added Bee class

---
+++ Project1/Project1/Bee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Project1
+{
+    class Bee
+    {
+        private Project1 game;   // the game
+        private Model beeModel; // bees model
+
+        public Bee(Project1 game)
+        {
+            this.game = game;
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        public void LoadContent(ContentManager content)
+        {
+            beeModel = content.Load<Model>("bee-rigid");
+        }
+
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void Update(GameTime gameTime)
+        {
+
+            // TODO: Add your update logic here
+
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void Draw(GraphicsDeviceManager graphics, GameTime gameTime)
+        {
+
+            // TODO: Add your drawing code here
+            DrawModel(graphics, beeModel, Matrix.Identity);
+        }
+
+
+        private void DrawModel(GraphicsDeviceManager graphics, Model model, Matrix world)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = transforms[mesh.ParentBone.Index] * world;
+                    effect.View = Matrix.CreateLookAt(new Vector3(30, 30, 30),
+                                                      new Vector3(0, 0, 0),
+                                                      new Vector3(0, 1, 0));
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(35),
+                        graphics.GraphicsDevice.Viewport.AspectRatio, 10, 10000);
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
